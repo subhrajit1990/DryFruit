baaf43939452a3660a2c9bc05f56954974793afa
subhrajit1990 <subhrajitsahu1990@gmail.com>
2022-04-04 02:37:53 +0530

    fifth commit for dry fruit web page :Subhrajit

--- src/server.js
+++ src/server.js
@@ -4,14 +4,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
-
 app.set('port', process.env.PORT || 8080);
-
-
-
-/*app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "build", "index.html"));
-});*/
 
 var server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
