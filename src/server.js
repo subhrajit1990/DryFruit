ed67456abb20df03facec7e839d77ddf39658d17
subhrajit1990 <subhrajitsahu1990@gmail.com>
2022-04-04 02:28:07 +0530

    fifth commit for dry fruit web page :Subhrajit

--- src/server.js
+++ src/server.js
@@ -1,57 +1,15 @@
-import { Server } from 'http';
-import Express from 'express';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import { match, RouterContext } from 'react-router';
-//import routes from './routes';
-
-// initialize the server and configure support for ejs templates
-const app = new Express();
-const server = new Server(app);
 var path = require('path');
+var express = require('express');
 
-// define the folder that will be used for static assets
-app.use(Express.static(path.join(__dirname, 'dist')));
+var app = express();
 
-// universal routing and rendering
-app.get('*', (req, res) => {
-  match(
-    { location: req.url },
-    (err, redirectLocation, renderProps) => {
+app.use(express.static(path.join(__dirname, 'public')));
 
-      // in case of error display the error message
-      if (err) {
-        return res.status(500).send(err.message);
-      }
+app.set('port', process.env.PORT || 8080);
 
-      // in case of redirect propagate the redirect to the browser
-      if (redirectLocation) {
-        return res.redirect(302, redirectLocation.pathname + redirectLocation.search);
-      }
 
-      // generate the React markup for the current route
-      let markup;
-      if (renderProps) {
-        // if the current route matched we have renderProps
-        markup = renderToString(<RouterContext {...renderProps}/>);
-      } else {
-        // otherwise we can render a 404 page
-        //markup = renderToString(<NotFoundPage/>);
-        res.status(404);
-      }
-
-      // render the index template with the embedded React markup
-      return res.render('index', { markup });
-    }
-  );
+var server = app.listen(app.get('port'), function() {
+  console.log('listening on port ', server.address().port);
 });
 
-// start the server
-const port = process.env.PORT || 8080;
-const env = process.env.NODE_ENV || 'production';
-server.listen(port, err => {
-  if (err) {
-    return console.error(err);
-  }
-  console.info(`Server running on http://localhost:${port} [${env}]`);
-});
+
