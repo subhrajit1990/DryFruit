fce45a3001863bf092eb6d3d38da9fbcbbeba172
subhrajit1990 <subhrajitsahu1990@gmail.com>
2022-04-04 03:25:37 +0530

    fifth commit for dry fruit web page :Subhrajit

--- src/server.js
+++ src/server.js
@@ -4,22 +4,10 @@ const app = express();
 const publicPath = path.join(__dirname, '..', 'dist');
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
+app.use(express.static(publicPath, '../public'))
 app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, '../src/index.html'));
 });
-
-
-// check extension and set mime type
-// HTTP Status: 200 : OK
-if (ext === 'css') {
-  // check for css
-  response.writeHead(200, {'Content-Type': 'text/css'});
-} else {
-  // Content Type: text/plain
-  // maybe also check .html type
-  res.writeHead(200, {'Content-Type': 'text/html'});
-}
-
 
 
 app.listen(port, () => {
