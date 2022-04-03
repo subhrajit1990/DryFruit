21e88c24aea8d9109fdc7ffe2a1a953a93ae78c8
subhrajit1990 <subhrajitsahu1990@gmail.com>
2022-04-04 03:29:51 +0530

    fifth commit for dry fruit web page :Subhrajit

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const publicPath = path.join(__dirname, '..', 'dist');
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 //app.use(express.static(publicPath, '../public'));
+app.use('/static', express.static(__dirname, '..','public'))
 app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, '../src/index.html'));
 });
