a727f51eec869559e9d742abf9606459ab798ee3
subhrajit1990 <subhrajitsahu1990@gmail.com>
2022-05-01 07:01:55 +0530

    Not found page addition :Subhrajit

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -2,10 +2,9 @@ import React from 'react';
 import { Route, HashRouter, NavLink, BrowserRouter as Router } from 'react-router-dom';
 
 
-export class NotFoundPage extends React.Component {
-  render() {
-    return (
-    	<>
+function NotFound(props){
+	return (
+			<>
 			<div className="search-area">
 				<div className="container">
 					<div className="row">
@@ -27,14 +26,21 @@ export class NotFoundPage extends React.Component {
 					<div className="row">
 						<div className="col-lg-8 offset-lg-2 text-center">
 							<div className="breadcrumb-text">
-								<p>Fresh and Organic</p>
-								<h1>Page not found</h1>
+								<p>{props.labelData}</p>
+								<h1>{props.headerData}</h1>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 		</>
+	)
+}
+
+export class NotFoundPage extends React.Component {
+  render() {
+    return (
+    	<NotFound labelData="Fresh and Organic" headerData="Page not found" />
     );
   }
 }
@@ -43,34 +49,7 @@ export class NoDataFoundPage extends React.Component {
   render() {
     return (
      	<>
-			<div className="search-area">
-				<div className="container">
-					<div className="row">
-						<div className="col-lg-12">
-							<span className="close-btn"><i className="fas fa-window-close"></i></span>
-							<div className="search-bar">
-								<div className="search-bar-tablecell">
-									<h3>Search For:</h3>
-									<input type="text" placeholder="Keywords"/>
-									<button type="submit">Search <i className="fas fa-search"></i></button>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div className="breadcrumb-section breadcrumb-bg">
-				<div className="container">
-					<div className="row">
-						<div className="col-lg-8 offset-lg-2 text-center">
-							<div className="breadcrumb-text">
-								<p>Fresh and Organic</p>
-								<h1>No data found !</h1>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+			<NotFound labelData="Fresh and Organic" headerData="No data found !" />
 			<div className="full-height-section error-section">
 				<div className="full-height-tablecell">
 					<div className="container">
@@ -82,8 +61,7 @@ export class NoDataFoundPage extends React.Component {
 									<p>Your cart is empty, please go back and add some ...</p>
 									<nav>
 										<NavLink  className="boxed-btn" to="/">Back to Home</NavLink>
-									</nav>
-									
+									</nav>								
 								</div>
 							</div>
 						</div>
