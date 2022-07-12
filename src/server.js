aa6b813a9ea48c40a04d3184332ca89618cad03c
subhrajit1990 <subhrajitsahu1990@gmail.com>
2022-07-12 14:42:16 +0530

    fav icon addition:Subhrajit

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const favicon = require("serve-favicon")
 app.use(express.static(publicPath));
 //app.use(express.static(publicPath, '../public'));
 //app.use('/static', express.static(__dirname, '..','public'));
-//app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+app.use(favicon(path.join(__dirname,'..', "public", "favicon.ico")));
 
 app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, '../src/index.html'));
