0c1c598a39d5309bff23e3a4d9e4498ff879a8f1
subhrajit1990 <subhrajitsahu1990@gmail.com>
2022-07-24 20:44:58 +0530

    changes for new design

--- src/components/constant.js
+++ src/components/constant.js
@@ -12,10 +12,10 @@ import App from './App';
 
 export const routes = (
     <Routes>
-	    <Route end path="/" element={<Home />}/>
+	    <Route  path="/" element={<Home />}/>
 	    <Route  path="/Contact" element={<Contact />}/>
 	    <Route  path="/About" element={<About />}/>
-	    <Route  name="ProductList" path="/ProductList" element={<ProductList />} />
+	    <Route  path="/ProductList" element={<ProductList />} />
 	    <Route  end name="ProductPageInterceptor" path="/ProductPageInterceptor" element={<ProductPageInterceptor />} />
 	    <Route  end name="ProductPage" path="/ProductPage" element={<ProductPage />} />
 	    <Route  end name="Cart" path="/Cart" element={<Cart />} />
