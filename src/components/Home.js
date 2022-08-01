991073b573cf3f935f7a7fa7de917a75cef8a466
subhrajit1990 <subhrajitsahu1990@gmail.com>
2022-08-01 10:57:48 +0530

    changes for new design

--- src/components/Home.js
+++ src/components/Home.js
@@ -43,7 +43,7 @@ export default class Home extends React.Component {
 		});
 	}
 
-	const handleChangeOptions = (event) => {
+	handleChangeOptions(event) {
     	setValue(event.target.value);
   	};
 
