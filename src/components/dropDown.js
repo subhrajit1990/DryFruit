bb636e57913a702826411e6916f49c5c08c868d6
subhrajit1990 <subhrajitsahu1990@gmail.com>
2022-08-01 15:26:58 +0530

    changes for new design

--- src/components/dropDown.js
+++ src/components/dropDown.js
@@ -6,16 +6,25 @@ const dropDown = (props) => {
   const options = props.productData;
 
   const [value, setValue] = React.useState('');
+  const [optionsType, setOptionsType] = React.useState('');
+
 
   const handleChange = (event) => {
     setValue(event.target.value);
+    options(event);
+    
   };
+
+  option(event){
+    setType(event.target.setOptionsType);
+  }
 
   return (
     <div>
       <Dropdown
         label="What do we eat?"
         options={options}
+        optionsType= {optionsType}
         value={value}
         onChange={handleChange}
       />
@@ -28,10 +37,10 @@ const Dropdown = ({ label, value, options, onChange }) => {
   return (
     <label>
       {label}
-      <select value={value} onChange={onChange}>
+      <select value={value}  optionsType= {optionsType} onChange={onChange}>
         {options.map((option) => (
-          <option value={option.price}>{option.variantName}</option>
-        ))}
+          <option value={option.price}  optionsType= {option.variantName}>{option.variantName}</option>
+        ))} 
       </select>
     </label>
   );
